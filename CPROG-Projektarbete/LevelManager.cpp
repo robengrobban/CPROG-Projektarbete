8f86c5338c841dd2c72e09ef061861bd97ffeea4
Robert Englund <kastanjen3@gmail.com>
2020-12-01 16:16:29 +0100

    Removed comments

--- CPROG-Projektarbete/LevelManager.cpp
+++ CPROG-Projektarbete/LevelManager.cpp
@@ -13,45 +13,31 @@ namespace engine {
 	}
 
 	void LevelManager::tick_current_level() {
-		// Tick the current level
 		this->levels.at(this->current_level)->draw_level();
 	}
 
 	void LevelManager::draw_current_level() {
-		// Draw the current level
 		this->levels.at(this->current_level)->draw_level();
 	}
 
 	int LevelManager::add_level(Level& level) {
-		// Get next level number
 		int next_level = this->levels.size();
-
-		// Add new level
 		this->levels.insert({next_level, &level});
-
-		// Return index
 		return next_level;
 	}
 
 	void LevelManager::assign_level(int number, Level& level) {
-		// Control so that the level number exists
 		if ( number >= this->levels.size() && number >= 0 ) {
 			throw std::out_of_range(std::string("Level access out of range, there is no index " + number));
 		}
 		else {
-			// Get old level
 			Level* old_level = this->levels[number];
-			
-			// Insert new level
 			this->levels[number] = &level;
-
-			// Delete old level
 			delete old_level;
 		}
 	}
 
 	void LevelManager::change_level(int number) {
-		// Control so that the level number exists
 		if (number >= this->levels.size() && number >= 0) {
 			throw std::out_of_range(std::string("Level access out of range, there is no index " + number));
 		}
@@ -78,21 +64,13 @@ namespace engine {
 	}*/
 
 	void LevelManager::remove_level(int number) {
-		// Control so that the level number exists
 		if (number >= this->levels.size()) {
 			throw std::out_of_range(std::string("Level access out of range, there is no index " + number));
 		}
 		else {
-			// Get level
 			Level* level = this->levels[number];
-			
-			// Remove entry
 			this->levels.erase(number);
-
-			// Delete
 			delete level;
-
-			// Rearrange level numbers
 			this->rearrange_levels();
 		}
 	}
@@ -106,7 +84,6 @@ namespace engine {
 	}
 
 	LevelManager::~LevelManager() {
-		// Delete map
 		std::map<int, Level*>::iterator it = this->levels.begin();
 		while (it != this->levels.end()) {
 			delete it->second;
@@ -116,25 +93,16 @@ namespace engine {
 	}
 
 	void LevelManager::rearrange_levels() {
-		// Go through every level and rearrange level number
 		this->current_level--;
 		int new_level_number = 0;
 
 		std::map<int, Level*>::iterator it = this->levels.begin();
 		while (it != this->levels.end()) {
 
-			// Check if rearrange is necesary
 			if ( it->first != new_level_number ) {
-
-				// Get the level
 				Level* level = it->second;
-
-				// Erase its position
 				it = this->levels.erase(it);
-
-				// Put it back in with new key
 				this->levels.insert({ new_level_number, level });
-
 			}
 			else {
 				++it;
