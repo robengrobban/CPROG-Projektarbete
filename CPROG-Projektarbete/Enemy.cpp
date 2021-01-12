a684898ab17025cf7b26a534c6233bb83c9dd3e5
ulfmal <ulfmal@users.noreply.github.com>
2021-01-12 18:21:31 +0100

    Changed enemy
    Changed to make enemy change direction not only when colliding with static objects, allowing for example enemies to bump into eachother without stopping there forever.

--- CPROG-Projektarbete/Enemy.cpp
+++ CPROG-Projektarbete/Enemy.cpp
@@ -29,20 +29,21 @@ namespace demo {
 	}
 
 	void Enemy::on_collision(GameObject& obj) {
-		if ( engine::StaticObject* s = dynamic_cast<engine::StaticObject*>(&obj) ) {
-			if (obj.get_bottom() == this->get_bottom() && obj.get_right() < this->get_right()) {
-				movement_speed = 3;
-			}
-			else if (obj.get_bottom() == this->get_bottom() && obj.get_left() > this->get_left()) {
-				movement_speed = -3;
-			}
-		}
-		else if ( Player* p = dynamic_cast<Player*>(&obj) ) {
+		
+		if ( Player* p = dynamic_cast<Player*>(&obj) ) {
 			if ( p->get_bottom() <= this->get_top() ) {
 				this->get_level().get_level_manager().get_current_level().remove_object(*this);
 			}
 			else {
 				p->die();
+			}
+		}
+		else if (obj.is_solid()) {
+			if (obj.get_bottom() == this->get_bottom() && obj.get_right() < this->get_right()) {
+				movement_speed = 3;
+			}
+			else if (obj.get_bottom() == this->get_bottom() && obj.get_left() > this->get_left()) {
+				movement_speed = -3;
 			}
 		}
 	}
